26d2719c0d52fb2bbaf7c20e9aff19b81c1cebdd
Amanda <amandajuhlsilberg@hotmail.com>
2025-03-26 10:39:33 +0100

    Add project files.

---
+++ FINALCarApp/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FINALCarApp
+{
+    enum FuelTypes
+    {
+        Petrol,
+        Diesel,
+        Electric,
+
+    }
+    internal class Car
+    {
+        private string _brand;
+        private string _model;
+        private int _year;
+        private char _geartype;
+        private enum FuelType;
+
+        // Setter
+        public
+
+
+    }
+}
---
+++ FINALCarApp/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace FINALCarApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the carpark of WONDERS!");
+
+
+
+
+
+
+            
+            Console.WriteLine("\nWhich option do you choose?\n\n1.Add a car!");
+
+
+
+
+        }
+
+       
+
+
+
+
+
+
+
+
+        
+
+
+    }
+}
